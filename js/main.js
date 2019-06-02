3ea7cb492e6028a9bbdf664fbedc708fe00cbda7
Matt Williams <mwilliamsii233203@gmail.com>
2019-06-02 13:33:53 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,5 +3,13 @@
 
     function init(){
         $('.dropdown-trigger').dropdown();
+        $('.sidenav').sidenav();
+        $("#arrow").on('click', scrollDown);
+    }
+
+    function scrollDown(){
+        $("html, body").animate({
+            scrollTop: $(".basics").offset().top
+        }, "slow");
     }
 })();
